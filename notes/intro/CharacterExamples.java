6377524a6ced64142348e081b1999b6515563717
Isaac <isaac.newt.list@icloud.com>
2019-09-10 09:08:30 -0500

    add characters example file

---
+++ notes/intro/CharacterExamples.java
@@ -0,0 +1,15 @@
+package java_project_2;
+
+public class CharacterExamples {
+	public static void main(String[] args) {
+		// Define the char variables
+		char exampleCharacter = 'A';
+		char exampleUnicodeChar = '\u0041';
+		String mathSetSymbol = "\ud835\udd6b";
+		
+		// Print out the characters defined above
+		System.out.println("Character A: " + exampleCharacter);
+		System.out.println("Unicode \\u0041: " + exampleUnicodeChar);
+		System.out.println("Unicode \\ud835\\udd6b: " + mathSetSymbol);
+	}
+}
