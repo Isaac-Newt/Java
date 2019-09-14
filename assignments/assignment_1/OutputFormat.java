3b9780c46120abae1f7eba536fd4f8af7294bcd4
isaac-newt <isaac.newt.list@icloud.com>
2019-09-14 13:15:23 -0500

    finish output formatting script

--- assignments/assignment_1/OutputFormat.java
+++ assignments/assignment_1/OutputFormat.java
@@ -6,6 +6,9 @@
 
 public class OutputFormat {
     public static void main(String[] args) {
-        
+        // Print the string using 
+        System.out.printf(
+        	"Twinkle, twinkle, little star,\n \tHow I wonder what you are!\n \t\tUp above the world so high,\n \t\tLike a diamond in the sky.\n Twinkle, twinkle, little star,\n \tHow I wonder what you are\n"
+        );
     }
 }
