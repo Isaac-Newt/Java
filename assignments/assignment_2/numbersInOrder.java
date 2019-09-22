001458a54b041445a3374bfaccd533f4fdd6311b
isaac-newt <isaac.newt.list@icloud.com>
2019-09-22 16:02:12 -0500

    add numbers in order

---
+++ assignments/assignment_2/numbersInOrder.java
@@ -0,0 +1,36 @@
+// Program that takes in three ints, and decides whether the ints
+// are entered in an increasing or decreasing order, or no order.
+
+import java.util.Scanner;
+
+class numbersInOrder {
+    public static void main(String[] args) {
+        // Open the Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Get three numbers from the user
+        System.out.print("Enter the first number: ");
+        int numberOne = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int numberTwo = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int numberThree = scanner.nextInt();
+
+        // Decide what order they are in
+        if (numberOne < numberTwo && numberTwo < numberThree) {
+            System.out.println("Increasing");
+        }
+
+        else {
+            if (numberOne > numberTwo && numberTwo > numberThree) {
+                System.out.println("Decreasing");
+            }
+            else {
+                System.out.println("Neither increasing nor decreasing");
+            }
+        }
+
+        // Close the Scanner
+        scanner.close();
+    }
+}
