b81c4c2a6dd94f43039c2925c957fbc959aa71c1
Isaac <isaac.newt.list@icloud.com>
2019-09-22 14:00:25 -0500

    add 2 more files for assignment 2

---
+++ assignments/assignment_2/replaceSubString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class replaceSubString {
+
+    public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Enter a string: ");
+      String string = sc.nextLine();
+
+      System.out.print("Enter replacement string: ");
+      String subString = sc.nextLine();
+
+      // TODO: find substrings that match subString
+
+      sc.close();
+
+    }
+
+}
