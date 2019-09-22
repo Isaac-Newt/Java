1063e1f34711b63bda4c3f938819ea9057d87d61
isaac-newt <isaac.newt.list@icloud.com>
2019-09-22 15:50:40 -0500

    make VSCodium happy about unused variable

--- notes/objects and classes/objects_classes/JavaClass.java
+++ notes/objects and classes/objects_classes/JavaClass.java
@@ -1,25 +1,25 @@
 package objects_classes;
 
 public class JavaClass {
-    
+
     // Declare attributes
     public int classroom;
     private int enrollmentCap;
     protected String instructor;
     protected String courseName;
     // This will be a default value, may be reassigned by the constructor
-    private String materials = "Book";    
-    
+    private String materials = "Book";
+
     public JavaClass(int room, int cap, String prof, String name) {
         // you can call another constructor, such calls should come first
         this(prof);
-        
+
         // initialize object attributes with above variables
         this.classroom = room;
         this.enrollmentCap = cap;
         this.courseName = name;
     }
-    
+
     public JavaClass(String prof) {
         // Another constructor, called from previous constructor
         this.instructor = prof;
@@ -29,15 +29,19 @@ public class JavaClass {
         // Prints course name as a string
         return this.courseName;
     }
-    
+
     // Define getters and setters for enrollmentCap
     // These MUST be public methods (otherwise basically useless)
     public int getEnrollmentCap() {
         return this.enrollmentCap;
     }
-    
+
     public int setEnrollmentCap(int newCap) {
         return this.enrollmentCap = newCap;
     }
-    
+
+    public String getMaterials() {
+        return this.materials;
+    }
+
 }
