9fecbde02687a52f384432ce8e881d11d3cf8a6f
isaac-newt <isaac.newt.list@icloud.com>
2019-09-23 18:55:11 -0500

    Finish the replace substring assignment

--- assignments/assignment_2/replaceSubString.java
+++ assignments/assignment_2/replaceSubString.java
@@ -1,20 +1,31 @@
+// Program to replace portions of a string with
+// a given sub-string.
+
 import java.util.Scanner;
 
 class replaceSubString {
 
     public static void main(String[] args) {
+        // Open the scanner
+        Scanner sc = new Scanner(System.in);
 
-      Scanner sc = new Scanner(System.in);
+        // Get the string and substring and its replacement
+        System.out.print("Enter a string: ");
+        String string = sc.nextLine();
 
-      System.out.print("Enter a string: ");
-      String string = sc.nextLine();
+        System.out.print("Enter a substring to be replaced: ");
+        String toReplace = sc.nextLine();
 
-      System.out.print("Enter replacement string: ");
-      String subString = sc.nextLine();
+        System.out.print("Enter replacement string: ");
+        String subString = sc.nextLine();
 
-      // TODO: find substrings that match subString
+        // Replace instances of the 
+        String newString = string.replaceAll(toReplace, subString);
 
-      sc.close();
+        // Print the new string to the console
+        System.out.println(newString);
+
+        sc.close();
 
     }
 
