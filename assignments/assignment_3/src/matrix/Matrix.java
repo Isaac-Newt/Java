2f5d9da83f89abf85a21d112cb07daf32084272f
isaac-newt <isaac.newt.list@icloud.com>
2019-09-30 19:13:51 -0500

    finished matrix (exercises 11 & 12)

--- assignments/assignment_3/src/matrix/Matrix.java
+++ assignments/assignment_3/src/matrix/Matrix.java
@@ -2,6 +2,8 @@
 
 package matrix;
 
+import java.util.Arrays;
+
 public class Matrix {
 	// Declare data members
 	private int Rows;
@@ -24,15 +26,68 @@ public class Matrix {
 		return Columns;
 	}
 	
+	// Getter for array of elements
+	public int[][] getElementsArray() {
+		return this.elementsArray;
+	}
+	
 	// Set the value of a cell in the matrix
 	public void setValue(int row, int column, int value) {
 		this.elementsArray[row][column] = value;
 	}
 	
 	// Add two matrices
-	// TODO: figure out how the heck to do this???
 	// If cannot add, display "Matrices cannot be added"
+	public Matrix addMatrices(Matrix matrix_1, Matrix matrix_2) {
+		
+		Matrix filler_matrix = new Matrix(0, 0);
+		
+		if (matrix_1.getRows() != matrix_2.getRows()) {
+			System.out.println("Matrices cannot be added");
+			return filler_matrix;
+		}
+		
+		else if (matrix_1.getColumns() != matrix_2.getColumns()) {
+			System.out.println("Matrices cannot be added");
+			return filler_matrix;
+		}
+		
+		else {
+			Matrix newMatrix = new Matrix(matrix_1.getRows(), matrix_1.getColumns());
+			
+			for (int i = 0; i < this.Rows; i++) {
+				for (int j = 0; j < this.Columns; j++) {
+					newMatrix.setValue(i, j, matrix_1.elementsArray[i][j] + matrix_2.elementsArray[i][j]);
+				}
+			}
+			return newMatrix;
+		}
+	}
 	
 	// Multiply two matrices
-	// TODO: figure out how the heck to do this???
+	public Matrix multiplyMatrices(Matrix matrix_1, Matrix matrix_2) {
+		
+		Matrix filler_matrix = new Matrix(0, 0);
+		
+		if (matrix_1.getRows() != matrix_2.getRows()) {
+			System.out.println("Matrices cannot be multiplied");
+			return filler_matrix;
+		}
+		
+		else if (matrix_1.getColumns() != matrix_2.getColumns()) {
+			System.out.println("Matrices cannot be multiplied");
+			return filler_matrix;
+		}
+		
+		else {
+			Matrix newMatrix = new Matrix(matrix_1.getRows(), matrix_1.getColumns());
+			
+			for (int i = 0; i < this.Rows; i++) {
+				for (int j = 0; j < this.Columns; j++) {
+					newMatrix.setValue(i, j, matrix_1.elementsArray[i][j] * matrix_2.elementsArray[i][j]);
+				}
+			}
+			return newMatrix;
+		}
+	}
 }
