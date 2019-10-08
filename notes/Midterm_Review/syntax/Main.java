3fc4af1d4aadd4cf0280a2050678892ae481502e
Isaac <isaac.newt.list@icloud.com>
2019-10-08 08:51:53 -0500

    Create Main.java

---
+++ notes/Midterm_Review/syntax/Main.java
@@ -0,0 +1,13 @@
+package examples;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create objects of class Syntax
+
+        // This will use the default constructor (100, 50)
+        Syntax example1 = new Syntax();
+
+        // This will use the other constructor
+        Syntax example2 = new Syntax(5, 10);
+    }
+}
