f99c1c17c4d00e6673fb04176a3b0db107cee088
Isaac <isaac.newt.list@icloud.com>
2019-11-12 08:48:50 -0600

    Update Employee.java

--- notes/Object_Class/example/Employee.java
+++ notes/Object_Class/example/Employee.java
@@ -4,7 +4,8 @@ public class Employee {
     
     // Declare data members
     private int ID;
-    private String Name;
+    private String firstName;
+    private String lastName;
 
     // Constructors
     public Employee() {
@@ -12,9 +13,10 @@ public class Employee {
     }
     
     // Populate data
-    public Employee(int id, String name) {
+    public Employee(int id, String fname, String lname) {
         this.ID = id;
-        this.Name = name;
+        this.firstName = fname;
+        this.lastName = lname;
     }
     
     // Getters and Setters
@@ -26,18 +28,26 @@ public class Employee {
         this.ID = newID;
     }
 
-    public String getName() {
-        return this.Name;
+    public String getFirstName() {
+        return this.firstName;
     }
     
-    public void setName(String newName) {
-        this.Name = newName;
+    public void setFirstName(String newName) {
+        this.firstName = newName;
+    }
+    
+    public String getLastName() {
+        return this.lastName;
+    }
+    
+    public void setLastName(String newName) {
+        this.lastName = newName;
     }
     
     // Override Object Class's default methods
     @Override
     public String toString() {
-        return this.Name;
+        return this.ID + this.firstName + this.lastName;
     }
     
     @Override
@@ -49,7 +59,9 @@ public class Employee {
         Employee testEmp = (Employee)emp;
         
         // Comparison
-        if (this.ID == testEmp.ID && this.Name.equals(testEmp.Name)) {
+        if (this.ID == testEmp.ID &&
+            this.firstName.equals(testEmp.firstName) &&
+            this.lastName.equals(testEmp.lastName)) {
             equals = true;
         }
         
