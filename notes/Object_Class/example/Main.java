185343fd9dfd12c8a548b9040305610f4ea7459e
Isaac <isaac.newt.list@icloud.com>
2019-11-12 08:18:43 -0600

    Create Main.java

---
+++ notes/Object_Class/example/Main.java
@@ -0,0 +1,15 @@
+package object_class_example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Create two employees
+        Employee emp1 = new Employee(1, "Daniel");
+        Employee emp2 = new Employee(2, "Daniel");
+        
+        // Use toString method
+        System.out.println(emp1);
+        System.out.println(emp2);
+    }
+
+}
