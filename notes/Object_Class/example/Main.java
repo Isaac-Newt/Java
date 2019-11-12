90978bc201af696899996f26fe5053cf17f819cb
Isaac <isaac.newt.list@icloud.com>
2019-11-12 08:49:33 -0600

    Update Main.java

--- notes/Object_Class/example/Main.java
+++ notes/Object_Class/example/Main.java
@@ -1,15 +1,40 @@
 package object_class_example;
 
+import java.util.Hashtable;
+
 public class Main {
 
     public static void main(String[] args) {
         // Create two employees
-        Employee emp1 = new Employee(1, "Daniel");
-        Employee emp2 = new Employee(2, "Daniel");
+        Employee emp1 = new Employee(1, "Daniel", "Smith");
+        Employee emp10 = new Employee(1, "Daniel", "Smith");
+        Employee emp2 = new Employee(2, "Daisy", "Jones");
         
         // Use toString method
         System.out.println(emp1);
         System.out.println(emp2);
+        
+        // Use equal method
+        System.out.println(emp1.equals(emp10));
+        
+        // Hash table example
+        
+        // Create the hash table
+        Hashtable hash = new Hashtable();
+        
+        // Create two theoretically identical keys
+        Key key1 = new Key(emp1.getFirstName(), emp1.getLastName());
+        Key key10 = new Key(emp10.getFirstName(), emp10.getLastName());
+        
+        // Create a third theoretically identical key manually
+        Key key3 = new Key("Daniel", "Smith");
+        
+        // Add two keys to the hash table
+        hash.put(key1, emp1);
+        hash.put(key10, emp10);
+        
+        // Look for the third key in the hash table
+        System.out.println(hash.containsKey(key3));
     }
 
 }
