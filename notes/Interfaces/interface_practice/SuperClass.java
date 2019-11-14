2d52092c42df3cded31bba5a20c4b7b725c08bc3
Isaac <isaac.newt.list@icloud.com>
2019-11-14 08:40:16 -0600

    Create SuperClass.java

---
+++ notes/Interfaces/interface_practice/SuperClass.java
@@ -0,0 +1,10 @@
+package interface_practice;
+
+public abstract class SuperClass {
+
+    // This method implementation takes precedence over the interface's
+    public void formalitiesWorld() {
+        System.out.println("Formalities World");
+    }
+    
+}
