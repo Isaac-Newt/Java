ff7a0bf32241c791e6c28855b37e9aa6a339b1b3
isaac-newt <isaac.newt.list@icloud.com>
2019-12-05 15:43:42 -0600

    finished assignment 10

--- assignments/assignment_7/src/presentation_exercises/Ex_10_menubars.java
+++ assignments/assignment_7/src/presentation_exercises/Ex_10_menubars.java
@@ -5,8 +5,69 @@ import java.awt.event.*;
 
 public class Ex_10_menubars {
 	
+	JMenu menu, submenu;
+	JMenuItem File, Edit, Copy, Paste, Help;
+	
 	public Ex_10_menubars() {
 		JFrame frame = new JFrame("Menubar Example");
+		
+		JMenuBar bar = new JMenuBar();
+		
+		menu = new JMenu("Menu");
+		submenu = new JMenu("Submenu");
+		
+		File = new JMenuItem("File");
+		Edit = new JMenuItem("Edit");
+		Copy = new JMenuItem("Copy");
+		Paste = new JMenuItem("Paste");
+		Help = new JMenuItem("Help");
+		
+		File.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "File is selected");
+			}
+		});
+		
+		Edit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Edit is selected");
+			}
+		});
+		
+		Copy.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Copy is selected");
+			}
+		});
+		
+		Paste.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Paste is selected");
+			}
+		});
+		
+		Help.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Help is selected");
+			}
+		});
+		
+		menu.add(File);
+		menu.add(Edit);
+		menu.add(Help);
+		
+		submenu.add(Copy);
+		submenu.add(Paste);
+		menu.add(submenu);
+		
+		bar.add(menu);
+		
+		frame.setJMenuBar(bar);
 		
 		frame.setSize(200, 400);
 		frame.setLayout(null);
